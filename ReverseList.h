acdae77fbdda39642981746812321e461548b1fc
Gary <799494760@qq.com>
2021-01-13 16:56:12 +0800

    反转链表

---
+++ ReverseList.h
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+//链表反转
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL){}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        ListNode *new_head = NULL;
+        while(head){
+            ListNode *next = head -> next;
+            head -> next = new_head;
+            new_head = head;
+            head = next;
+        }
+        return new_head;
+    }
+};
